1530204352e8afeb073f10fa8e478df89deed2b7
Seonaid <seonaidl@gmail.com>
2014-12-19 11:10:32 -0400

    changed password on sql back to what was working

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path      = require("path");
 //var Sequelize = require("sequelize");
 var env       = process.env.NODE_ENV || "development";
 var Sequelize = require('sequelize')
-    , sequelize = new Sequelize('nerdique', 'seonaid', '',{
+    , sequelize = new Sequelize('nerdique', 'seonaid', 'arbitrary',{
       dialect: "postgres",
       port: 5432,
     })
