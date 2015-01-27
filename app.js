bf1211454338cebc083f19c7ce36607c594e68e9
Seonaid <seonaidl@gmail.com>
2015-01-27 17:34:55 -0400

    tranferred extremely basic session managment from Bones application. css not working with new files.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,6 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({secret: 'supersecrettoken_replace'}));
 
 app.use('/', routes);
 //app.use('/login', routes);
